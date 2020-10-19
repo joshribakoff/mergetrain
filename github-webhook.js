b72da94a8cce86726e7e0a7d6232d36ef7768cfe
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-10-19 08:06:29 -0700

    test(adds failing test for signatures)

--- github-webhook.js
+++ github-webhook.js
@@ -1,4 +1,5 @@
 module.exports = (req, res) => {
+    console.log(req.headers)
   let payload;
   if (Object.keys(req.body).length === 1 && undefined !== req.body.payload) {
     payload = JSON.parse(req.body.payload);
--- github-webhook.test.js
+++ github-webhook.test.js
@@ -2,12 +2,22 @@ const reviewRequested = require("./review-requested-team.json");
 const app = require("./app");
 const request = require("supertest");
 
-it("responds with hi world", () => 
+it("rejects invalid key", () =>
   request(app)
-    .post("/github-webhook")
+  .post("/github-webhook")
+  .set("x-hub-signature-256", 'invalid')
     .send(reviewRequested)
     .expect("Content-Type", /html/)
-    .expect("Content-Length", "8")
+    .expect(403)
+    .then((response) => expect(response.text).toEqual("not authorized")));
+
+it("validates the key", () => {
+//   const secret = "oeuoeu";
+  return request(app)
+  .post("/github-webhook")
+  .set("x-hub-signature-256", "sha256=bbdf8a7661ca64d0302dd6f22833b999c93cb61bd1080ae74183d0b9077c170e")
+    .send(reviewRequested)
+    .expect("Content-Type", /html/)
     .expect(200)
-    .then(response => expect(response.text).toEqual('hi world'))
-);
+    .then((response) => expect(response.text).toEqual("hi world"));
+});
