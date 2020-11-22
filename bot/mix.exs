635159779a32c72577ce0e94faf1d5e775e91bb9
Josh Ribakoff <josh.ribakoff@gmail.com>
2020-11-22 10:07:18 -0800

    feat(installs plug_cowboy)

--- bot/mix.exs
+++ bot/mix.exs
@@ -24,6 +24,8 @@ defmodule Bot.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:plug_cowboy, "~> 2.0"}, # This will pull in Plug AND Cowboy
+      {:poison, "~> 3.1"} # Latest version as of this writing
     ]
   end
 end
